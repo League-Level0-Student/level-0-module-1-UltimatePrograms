7bfe2e510a6e2f524a851b5cd6f7982d7a9b3e2c
hapev <league@WTS-iMac-11.attlocal.net>
2018-08-08 12:01:09 -0700

    day 3 summer slam

---
+++ src/badger.java
@@ -0,0 +1,32 @@
+
+public class badger {
+	public static void main(String[] args) {
+		String b = ("badger");
+		String m = ("mushroom");
+		String s = ("snake");
+		for (int i = 0; i < 9; i++) {
+			System.out.println(b);
+		}
+		System.out.println(m);
+		System.out.println(m);
+		System.out.println("A");
+		for (int i = 0; i < 9; i++) {
+			System.out.println(b);
+		}
+		System.out.println(m);
+		System.out.println(m);
+		for (int i = 0; i < 9; i++) {
+			System.out.println(b);
+		}
+		System.out.print("mush-");
+		System.out.println(m);
+		for (int i = 0; i < 9; i++) {
+			System.out.println(b);
+		}
+		System.out.print("A " + s + ",");
+		System.out.println("A " + s + ",");
+		System.out.print("Snaaake!");
+		System.out.println("Snaaake!");
+		System.out.println("Oooh, it's a snake");
+	}
+}
--- src/riddle.java
+++ src/riddle.java
@@ -1,12 +1,39 @@
 import javax.swing.JOptionPane;
 
 public class riddle {
-public static void main(String[] args) {
-	int score = 0;
-	String rid = JOptionPane.showInputDialog("Take off my skin - I won't cry, but you will! What am I?");
-if (rid.equals("onion")) {
-	score ++;
-}
-else ()
-}
+	public static void main(String[] args) {
+		int score = 0;
+		String rid = JOptionPane.showInputDialog("I'm tall when I'm young and I'm short when I'm old. What am I?");
+		if (rid.equals("candle")) {
+			score++;
+		} else {
+			score--;
+
+		}
+		if (score >= 0) {
+			speak("your score is" + score);
+		} else {
+			speak("your score is negative" + score);
+		}
+		String rid1 = JOptionPane.showInputDialog("Take off my skin - I won't cry, but you will! What am I?");
+		if (rid1.equals("onion")) {
+			score++;
+		} else {
+			score--;
+
+		}
+		if (score >= 0) {
+			speak("your score is" + score);
+		} else {
+			speak("your score is negative" + score);
+		}
+	}
+
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
